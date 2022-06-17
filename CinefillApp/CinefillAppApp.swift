771b49ef4c4d46edab4405dfb0d0b036ba63a290
Nouster <djebali.mohamed88@gmail.com>
2022-06-17 16:34:09 +0200

    Initial Commit

---
+++ CinefillApp/CinefillAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinefillAppApp.swift
+//  CinefillApp
+//
+//  Created by Mohamed Djebali on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CinefillAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
