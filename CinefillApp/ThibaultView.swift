b4dd35f5e66f60eb41de56e0eae8a299efd80b45
ThibaultGdf <thibaultgodef81@gmail.com>
2022-06-17 16:55:10 +0200

    Changement du texte

---
+++ CinefillApp/ThibaultView.swift
@@ -0,0 +1,21 @@
+//
+//  ThibaultView.swift
+//  CinefillApp
+//
+//  Created by Apprenant 20 on 17/06/2022.
+//
+
+import SwiftUI
+
+struct ThibaultView: View {
+    var body: some View {
+        Text("Hello à tous")
+        
+    }
+}
+
+struct ThibaultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThibaultView()
+    }
+}
