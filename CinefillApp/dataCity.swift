34b9847f1f9e3bf0b72b499c3a6efc6fd78d4318
Skyynek <moeson.jonathan@gmail.com>
2022-06-20 16:08:59 +0200

    First commit

---
+++ CinefillApp/dataCity.swift
@@ -0,0 +1,14 @@
+//
+//  dataCity.swift
+//  CinefillApp
+//
+//  Created by Apprenant 06 on 20/06/2022.
+//
+
+import SwiftUI
+
+struct City: Identifiable{
+    var id = UUID()
+        var cityName : String
+        var zipCode : Int
+}
