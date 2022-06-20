22bb781afc220ef979ac77805db51e964220a655
Nouster <djebali.mohamed88@gmail.com>
2022-06-20 10:08:23 +0200

    Created new view ===> BoxOfficeView

---
+++ CinefillApp/BoxOfficeView.swift
@@ -0,0 +1,20 @@
+//
+//  BoxOfficeView.swift
+//  CinefillApp
+//
+//  Created by Mohamed Djebali on 20/06/2022.
+//
+
+import SwiftUI
+
+struct BoxOfficeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BoxOfficeView_Previews: PreviewProvider {
+    static var previews: some View {
+        BoxOfficeView()
+    }
+}
