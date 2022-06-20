e9039653668505bddf1eb4270ffb4a78123e5b48
Nouster <djebali.mohamed88@gmail.com>
2022-06-20 09:59:17 +0200

    Created my view ===> HomePageView

---
+++ HomePageView.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageView.swift
+//  CinefillApp
+//
+//  Created by Mohamed Djebali on 20/06/2022.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
+    }
+}
