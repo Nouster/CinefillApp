a37185735c3184d5235471e13363f5a1a49905b0
Nouster <djebali.mohamed88@gmail.com>
2022-06-21 15:28:49 +0200

    Created new branch for HomePageView

---
+++ CinefillApp/HomePageView.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageView.swift
+//  CinefillApp
+//
+//  Created by Mohamed Djebali on 21/06/2022.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct HomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
+    }
+}
