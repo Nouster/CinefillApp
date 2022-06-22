a58ba9fa0380402cb4280868e56f0c89446335e0
Skyynek <moeson.jonathan@gmail.com>
2022-06-22 09:59:00 +0200

    Complete dataEvents

--- CinefillApp/eventsData.swift
+++ CinefillApp/eventsData.swift
@@ -4,31 +4,39 @@
 //
 //  Created by Mohamed Djebali on 20/06/2022.
 //
+import SwiftUI
 
-import Foundation
 
 struct Events : Identifiable{
     var id = UUID()
     var eventsName: String
     var eventsPictures: String
     var eventsDescription: String
-    var eventsDateStart: String
-    var eventsDateEnd : String
+    var eventsDateStart: String?
+    var eventsDateEnd : String?
     var eventsLocation: String
+    var enventsAdress : String?
+    var eventsCategorie : String
 }
 
-var eventsArray: [Events] = [
+
+var eventsPreviewArray = [
     
-    Events (eventsName: "Printemps du cinéma", eventsPictures: "printempsducinema", eventsDescription: "Le Printemps du Cinéma revient du dimanche 20 au mardi 22 mars 2022 ! Pendant trois jours, un tarif exceptionnel pour l'ensemble des spectateurs : 4€ la séance dans tous les cinéma participants. Une belle occasion de profiter pleinement de toute la richesse de la programmation : tous les films sont concernés, qu'il s'agisse de l'ensemble des nouveautés ou des films encore à l'affiche.", eventsDateStart: "20/03/2022", eventsDateEnd: "22/03/2022", eventsLocation: "France"),
+    Events(eventsName: "Elvis", eventsPictures: "elvis", eventsDescription: "La vie et l'oeuvre musicale d'Elvis Presley à travers le prisme de ses rapports complexes avec son mystérieux manager, le colonel Tom Parker. Le film explorera leurs relations sur une vingtaine d'années, de l'ascension du chanteur à son statut de star inégalé, sur fond de bouleversements culturels et de la découverte par l'Amérique de la fin de l'innocence.", eventsDateStart: nil, eventsDateEnd: nil, eventsLocation: "France",enventsAdress: nil, eventsCategorie: "Avant-Première"),
     
-    Events (eventsName: "Cinéma sous les étoiles", eventsPictures: "souslesetoiles", eventsDescription: "Découvrez en famille, le cinéma sous les étoiles au Parc De La Tête D'Or. À partir de 20h. \nMia a onze ans quand elle noue une relation hors du commun avec Charlie, un lion blanc né dans la ferme de félins de ses parents en Afrique du Sud. Tous deux grandissent comme frère et sœur et deviennent vite inséparables. Trois ans plus tard, Charlie est devenu un lion imposant. Mia découvre alors le secret que cache la ferme : son père vend les lions à des « chasseurs de trophées ». Mia n’aura désormais qu’une obsession : sauver Charlie, à tout prix.", eventsDateStart: "15/07/2022", eventsDateEnd: "15/07/2022", eventsLocation: "Parc De La Tête D'Or"),
+    Events(eventsName: "Printemps du cinéma", eventsPictures: "printempsducinema", eventsDescription: "Un tarif exceptionnel pour l'ensemble des spectateurs : 4€ la séance dans tous les cinéma participants. Une belle occasion de profiter pleinement de toute la richesse de la programmation : tous les films sont concernés, qu'il s'agisse de l'ensemble des nouveautés ou des films encore à l'affiche.", eventsDateStart: "09/05/2022", eventsDateEnd: "29/05/2022", eventsLocation: "Toulouse", enventsAdress: nil, eventsCategorie: "Promotion"),
     
-    Events (eventsName: "Up' Colomiers, c'est parti !", eventsPictures: "colombier", eventsDescription: "Conférences, échanges, ateliers linguistiques, culinaires, ciné-débat... Le programme sera riche en rencontres et en connaissances. Autant d'occasions propices aux échanges et au développement du lien social.", eventsDateStart: "03/07/2022", eventsDateEnd: "05/07/2022", eventsLocation: "Mairie de Toulouse"),
+    Events(eventsName: "Cinéma sous les étoiles: Le Roi Lion", eventsPictures: "souslesétoilesleroilion", eventsDescription: "Pour cette première séance de Ciné sous les étoiles, découvrez en famille le film **Le roi lion,** un film de Jon Favreau.", eventsDateStart: "12/07/2022", eventsDateEnd: "12/07/2022", eventsLocation: "Toulouse",enventsAdress: "Parc Duroch - Rue Gilet, 31770 Colomiers 31770 Colomiers", eventsCategorie: "Projection"),
     
-    Events (eventsName: "Le cinéma fait sa fête de la musique", eventsPictures: "elvis", eventsDescription: "À partir de 19h : bistrot concert au Central Club avec le groupe Archer qui jouera les classiques de la musique Pop-Rock des années 60 à nos jours. L'évenement débutera à 20h.", eventsDateStart: "21/06/2022", eventsDateEnd: "21/06/2022", eventsLocation: "Cinéma Le Grand Central"),
+    Events(eventsName: "Up' Colomiers, c'est parti !", eventsPictures: "colomiers", eventsDescription: "Conférences, échanges, ateliers linguistiques, culinaires, ciné-débat... Le programme sera riche en rencontres et en connaissances. Autant d'occasions propices aux échanges et au développement du lien social.", eventsDateStart: "19/03/2022", eventsDateEnd: "24/06/2022", eventsLocation: "Toulouse",enventsAdress: "4 Pl. Alex Raymond, 31770 Colomiers", eventsCategorie: "Conference"),
     
-    Events (eventsName: "Cinéma : Vente d'affiches", eventsPictures: "ventedaffiche", eventsDescription: "Venez fouiller dans les stocks d’affiches de films programmés depuis l’ouverture du Grand Central et repartez avec vos préférées ! Début des ventes à 18h.", eventsDateStart: "10/07/2022", eventsDateEnd: "10/07/2022", eventsLocation: "Cinéma Le Grand Central"),
+    Events(eventsName: "Première Vague", eventsPictures: "premirevague", eventsDescription: "L'équipe du Comœdia vous propose de découvrir en avant-première une sélection de films cannois présentés cette année dans différentes compétitions du festival.", eventsDateStart: "15/06/2022", eventsDateEnd: "21/06/2022", eventsLocation: "Lyon",enventsAdress: nil, eventsCategorie: "Festival"),
     
-    Events (eventsName: "Cinéma sous les étoiles : Le Roi Lion", eventsPictures: "roilion", eventsDescription: "Pour cette première séance de Ciné sous les étoiles, découvrez en famille le film à partir de 20h. Un film de Jon Favreau. Avec les voix de Rayane Bensetti, Anne Sila, Jean Reno...\n Synopsis : le jour se lève sur la savane africaine. Les animaux de toutes sorte engagent un périple vers un piton rocheux. C’est le jour de la présentation aux animaux de Simba, fils du roi lion Mufasa et de la reine lionne Sarabi. Mufasa rend ensuite visite à son frère Scar, qui n’est pas venu à la cérémonie. Ce dernier est jaloux de son frère et convoite son trône. Avec les hyènes, il va échafauder un plan pour le débarrasser de son frère et de Simba, pour s’en emparer.", eventsDateStart: "28/06/2022", eventsDateEnd: "29/06/2022", eventsLocation: "Parc De La Tête D'or")
+    Events(eventsName: "Le Chameau et le meunier", eventsPictures: "Lechameauetlemeunier", eventsDescription: "Un meunier fait travailler un chameau pour moudre le grain. Mais un jour, le chameau tombe malade et le meunier le remplace par un engin à moteur. Cette solution semble parfaite mais derrière la pudeur apparente des personnages, chaque se rend compte de l'attachement qu'il a pour l'autre. Programme de 3 courts métrages : Le Robot et le fermier (Restauré) Les Oiseaux blancs (Restauré)  Le Chameau et le meunier (15 minutes) " , eventsDateStart: "01/07/2022", eventsDateEnd: nil, eventsLocation: "Lyon",enventsAdress: nil, eventsCategorie: "Projection"),
+    
+    Events(eventsName: "Yuku et la fleur de l'Himalaya", eventsPictures: "yukuetlafleurdelhimalaya", eventsDescription: "En haut des plus hautes montagnes de la terre vit une plante qui se nourrit de la plus parfaite lumière du soleil. Elle s'appelle… la fleur de l'Himalaya. Yuku quitte sa famille pour partir à la recherche de cette fleur à la lumière éternelle. Elle veut l'offrir à sa grand-mère qui a annoncé qu'elle devra bientôt partir avec la petite taupe aveugle dans les méandres de la terre. Mais pour la trouver, il y a un long voyage à parcourir, semé d'obstacles. Il faut traverser le terrible domaine des rats d'égouts sous le château, la prairie aux corbeaux, cruels et voraces, la forêt enchantée où l'on se perd. Et, surtout, le petit pont de la peur, qui est gardé par le loup ! Mais sur son parcours, grâce à sa musique et à ses chansons, Yuku va se faire beaucoup d'amis. Ils sont le bien le plus précieux pour réussir l'aventure de la vie.", eventsDateStart: "28/08/2022", eventsDateEnd: nil, eventsLocation: "Lyon",enventsAdress: nil, eventsCategorie: "Avant-Première"),
+    
+    Events(eventsName: "L’Été en cinémascope 2022", eventsPictures: "etecinemascope", eventsDescription: "endez-vous estival de l'Institut Lumière depuis plus de 20 ans, les projections en plein air sont l’occasion, dès la nuit tombée, de découvrir un film sur grand écran : classiques, comédies, polars… " , eventsDateStart: "28/06/2022", eventsDateEnd: "30/08/2022", eventsLocation: "Lyon" ,enventsAdress: "Place Ambroise Courtois, Lyon 8e", eventsCategorie: "Festival")
     
 ]
+
