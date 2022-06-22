b783a89fc5ea0816cb679fb0caa4ebfb5e0b927a
Nouster <djebali.mohamed88@gmail.com>
2022-06-22 16:34:39 +0200

    Added carousel

--- CinefillApp/HomePageView.swift
+++ CinefillApp/HomePageView.swift
@@ -2,59 +2,70 @@
 //  HomePageView.swift
 //  CinefillApp
 //
-//  Created by Mohamed Djebali on 21/06/2022.
+//  Created by Mohamed Djebali on 22/06/2022.
 //
 
 import SwiftUI
 
 struct HomePageView: View {
+    private var numberOfImages: Int = 5
+    private let  timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    @State private var  currentIndex = 0
+    func previous (){
+        withAnimation{
+        currentIndex = currentIndex > 0 ? currentIndex - 1 : numberOfImages - 1
+    }
+    }
+    func next() {
+        withAnimation{
+        currentIndex = currentIndex < numberOfImages ? currentIndex + 1 : 0
+        }
+    }
+    
+    var controls: some View {
+        HStack{
+            Button {
+                previous()
+            } label: {
+                Image(systemName: "chevron.left")
+            }
+            Spacer()
+                .frame(width: 100)
+            Button {
+                next()
+            } label: {
+                Image(systemName: "chevron.right")
+            }
+
+        }.accentColor(.primary)
+    }
+    
     var body: some View {
         ZStack{
-            
             Color("backgroundcolorapp")
-            
-            Button {
-                print("Some text")
-            } label: {
-                HStack{
-                Image(systemName: "location.fill")
-                    .font(.system(size: 16))
-                    .foregroundStyle(LinearGradient (colors: [Color("cinefillorange"), .red], startPoint: .top, endPoint: .bottom))
-                
-            
-                        Text("Toulouse Saint-Alban")
-                            .foregroundColor(.white)
-                            .font(.system(size: 16))
-                        .fontWeight(.semibold)
-                        
-                  
-                    
-                    
-                    }
-                    
-                
-                
-                    
-                    
-                        
-                    }
-            
+        GeometryReader { proxy in
            
-                    
-                }
-            
-               
-                
-                
+            TabView (selection: $currentIndex) {
+                ForEach ( 1..<numberOfImages) {
+                    num in Image ("\(num)")
+                        .resizable()
+                        .scaledToFill()
+                        .tag(num)
+                    }
+            } .tabViewStyle(PageTabViewStyle())
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+                .padding()
+                .frame(width: proxy.size.width, height: proxy.size.height / 3)
+                .onReceive(timer, perform: { _ in
+                    next()
+                })
+                controls
             }
-            
         }
+}
+}
 
-    
-
-
-
-struct HomePageView_Previews: PreviewProvider {
+struct TestView_Previews: PreviewProvider {
     static var previews: some View {
         HomePageView()
     }
