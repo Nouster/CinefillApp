681ad8f026940d78d8514a2592aa5473767e67cd
Nouster <djebali.mohamed88@gmail.com>
2022-06-23 11:01:48 +0200

    Added scroll view

--- CinefillApp/HomePageView.swift
+++ CinefillApp/HomePageView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct HomePageView: View {
-    private var numberOfImages: Int = 5
+    private var numberOfImages: Int = 6
     private let  timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
     @State private var  currentIndex = 0
     func previous (){
         withAnimation{
-        currentIndex = currentIndex > 0 ? currentIndex - 1 : numberOfImages - 1
-    }
+            currentIndex = currentIndex > 0 ? currentIndex - 1 : numberOfImages - 1
+        }
     }
     func next() {
         withAnimation{
-        currentIndex = currentIndex < numberOfImages ? currentIndex + 1 : 0
+            currentIndex = currentIndex < numberOfImages ? currentIndex + 1 : 0
         }
     }
     
@@ -27,44 +27,139 @@ struct HomePageView: View {
             Button {
                 previous()
             } label: {
-                Image(systemName: "chevron.left")
+                
             }
             Spacer()
                 .frame(width: 100)
             Button {
                 next()
             } label: {
-                Image(systemName: "chevron.right")
+                
             }
-
+            
         }.accentColor(.primary)
     }
     
     var body: some View {
         ZStack{
+            
             Color("backgroundcolorapp")
-        GeometryReader { proxy in
-           
-            TabView (selection: $currentIndex) {
-                ForEach ( 1..<numberOfImages) {
-                    num in Image ("\(num)")
-                        .resizable()
-                        .scaledToFill()
-                        .tag(num)
+            
+            VStack {
+                
+                
+                
+                
+                
+                
+                VStack {
+                    Button {
+                        print("Something")
+                    } label: {
+                        Image(systemName: "location.fill")
+                            .font(.system(size: 20))
+                            .foregroundColor(Color("cinefillorange"))
+                            .offset(x: 90, y: 20)
+                        Text("Toulouse Saint-Alban")
+                            .foregroundColor(.white)
+                            .offset(x: 80, y: 20)
+                        
+                        
+                        
+                        
                     }
-            } .tabViewStyle(PageTabViewStyle())
-                .clipShape(RoundedRectangle(cornerRadius: 5))
-                .padding()
-                .frame(width: proxy.size.width, height: proxy.size.height / 3)
-                .onReceive(timer, perform: { _ in
-                    next()
-                })
-                controls
+                    
+                    Divider()
+                        .frame(width: 157, height: 2)
+                        .background(Color("cinefillorange"))
+                        .offset(x: 97, y: 10)
+                    
+                    Text("Les films à l'affiche")
+                    
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .offset(x: -90, y: 20)
+                        .padding()
+                    
+                    
+                    Divider()
+                        .frame(width: 160, height: 2)
+                        .background(Color("cinefillorange"))
+                        .offset(x: -90, y: 10)
+                    
+                    
+                }
+                
+                
+                
+                
+                GeometryReader { proxy in
+                    
+                    
+                    
+                    TabView (selection: $currentIndex) {
+                    ForEach ( 1..<numberOfImages) {
+                            num in Image ("\(num)")
+                                .resizable()
+                                .scaledToFill()
+                            
+                                .tag(num)
+                        }
+                    } .tabViewStyle(PageTabViewStyle())
+                        .clipShape(RoundedRectangle(cornerRadius: 25))
+                        .padding()
+                        .frame(width: proxy.size.width, height: proxy.size.height / 3)
+                        .onReceive(timer, perform: { _ in
+                            next()
+                        })
+                    controls
+                }
+                
+            }
+            VStack{
+                Text("Les séances les moins chères")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .offset(x: -50)
+                Divider()
+                    .frame(width: 240, height: 2)
+                    .background(Color("cinefillorange"))
+                    .offset(x: -50)
+                ScrollView(.horizontal){
+                    
+                    HStack{
+                        
+                        Button {
+                            print("someting")
+                        } label: {
+                            Image("jurassic")
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(15)
+                                .frame(width: 100, height: 100)
+                                .offset(x: 10)
+                                
+                        }
+                        Button {
+                            print("someting")
+                        } label: {
+                            Image("jurassic")
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(15)
+                                .frame(width: 150, height: 100) .offset(x: -30)
+                       
+                        
+                        
+                    }
+                }
+                
+                
             }
         }
+    }
 }
 }
-
 struct TestView_Previews: PreviewProvider {
     static var previews: some View {
         HomePageView()
