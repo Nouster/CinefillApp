99ffc7f7d187918613e3f33b173ce54c078c2ef4
missbel <louisa.belalia31@gmail.com>
2022-06-28 14:22:06 +0200

    added credit card button payment plus textfield

--- Payment.swift
+++ Payment.swift
@@ -12,7 +12,8 @@ struct Payment: View {
     @State var credit: String = ""
     @State var month: String = ""
     @State var checkout: String = ""
-    
+    @State private var isSelected: Bool = false
+    @State private var iNotSelected: Bool = false
     var body: some View {
         
         ZStack{
@@ -25,35 +26,105 @@ struct Payment: View {
                 
                 Text("UCG Cinéma la roquette")
                     .foregroundColor(.white)
+                    .font(.system(size: 20))
                 
                 Divider()
-                    .frame(width: 500, height: 2)
+                    .frame(width: 500, height: 5)
                     .background(.orange)
-                Image("carte 1")
                 
-                    .padding()
+//                    .padding()
+                
+                Image("top")
+                    .resizable()
+                    .frame(width: 300, height: 150)
+                
                 Text("TOP GUN : Maverick")
-                    .foregroundColor(.white)
-                Text("7,00 e")
+                    .foregroundColor(.white
+                    )
+                Text("7,00 €")
                 
                     .foregroundColor(.white)
-                    .padding()
+                
                 
                 Divider()
-                    .frame(width: 500, height: 2)
+                    .frame(width: 400, height: 5)
                     .background(.orange)
                 
                 
-                
+//                    .padding()
                 HStack{
-                    Image(systemName: "creditcard")
-                    Image(systemName: "creditcard")
-                    Image(systemName: "creditcard")
-                    Image(systemName: "creditcard")
+                    Image("CB")
+                        .resizable()
+                        .frame(width: 50, height: 40)
+                        .cornerRadius(5)
+                    
+                    Image("VISA")
+                        .resizable()
+                        .frame(width: 50, height: 40)
+                        .cornerRadius(5)
+                    Image( "MC")
+                        .resizable()
+                        .frame(width: 50, height: 40)
+                        .cornerRadius(5)
+                    Image( "AP")
+                        .resizable()
+                        .frame(width: 50, height: 40)
+                        .cornerRadius(5)
+                    
+                }.padding()
+                
+                VStack{
+                    TextField("Numero de carte ", text: $credit )
+                        .padding()
+                        .background(Color.orange.opacity(0.7).cornerRadius(10))
+                        .foregroundColor(.white)
+                  
+                        .frame(width: 300, height: 50)
+                    //
+                    
+                    TextField("MM/AA", text: $month )
+                        
+                        .padding()
+                        .background(Color.orange.opacity(0.7).cornerRadius(10))
+                        .foregroundColor(.white)
+//
+                        .frame(width: 300, height: 50)
+                        .padding()
+                    
+                    TextField("123", text: $checkout )
+                        .padding()
+                        .background(Color.orange.opacity(0.7).cornerRadius(10))
+                        .foregroundColor(.white)
+                      
+                        .frame(width: 300, height: 50)
+                    
+                    VStack{
+                        HStack{
+                    Toggle("Enregistrer ma carte ", isOn: $isSelected)
+                            .foregroundColor(.white)
+                        }.padding()
+                    }.padding(.horizontal, 65)
                     
                     
+                    Button{
+                   } label: {
+                    ZStack{
+                    Rectangle()
                     
+                        .frame(width: 300, height: 50)
                     
+                        .foregroundColor(.orange.opacity(0.7))
+                
+                        .cornerRadius(15)
+                        
+                        
+                        Text("Payer 7,00€")
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                    }
+                   }.padding()
+                    Text("ANNULER LE PAIMENT ")
+                        .font(.system(size: 14))
                 }.padding()
                 
                 
@@ -61,6 +132,7 @@ struct Payment: View {
         }
     }
 }
+
 
 
 
