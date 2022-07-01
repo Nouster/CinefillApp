c3952ca2d8a96604d6ed2db884a1937adae5da08
Nouster <djebali.mohamed88@gmail.com>
2022-07-01 16:01:55 +0200

    added new launch screen

--- CinefillApp/CinefillAppApp.swift
+++ CinefillApp/CinefillAppApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct CinefillAppApp: App {
     var body: some Scene {
         WindowGroup {
-            CustomTabBarView(viewRouter:  ViewRouter())
+            LaunchScreenView()
+            
         }
+      
     }
 }
